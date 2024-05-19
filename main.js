1da0e2c1bc4188384fbd0d36fdb0054a6ada8e65
javeriam911 <javeriam911@gmail.com>
2024-05-19 20:40:20 +0500

    if-else-&-ternary-operator

---
+++ main.js
@@ -0,0 +1,58 @@
+var isCompleted = true;
+if (isCompleted) {
+    console.log("welcome");
+}
+else {
+    console.log("bye");
+}
+;
+var age = 18;
+if (age > 18) {
+    console.log("you are allowed");
+}
+else {
+    console.log("you are not allowed");
+}
+var haveNiceJob = true;
+var canCook = false;
+if (haveNiceJob && canCook) {
+    console.log("rishta pakkA");
+}
+else {
+    console.log("rejected");
+}
+;
+var isHandsome = true;
+var isRich = false;
+if (isHandsome || isRich) {
+    console.log("rishta pakka!");
+}
+else {
+    console.log("rejected");
+}
+var percentage = 44;
+if (percentage >= 80 && percentage <= 100) {
+    console.log("Your grade is A+");
+}
+else if (percentage >= 70 && percentage < 80) {
+    console.log("your grade is A");
+}
+else if (percentage >= 60 && percentage < 70) {
+    console.log("your grade is B");
+}
+else if (percentage <= 50 && percentage < 60) {
+    console.log("your grade is C");
+}
+else {
+    console.log("your are fail");
+}
+////TERNARY OPERATOR 
+var isHungry = true;
+var snack = isHungry ? "apple" : "water";
+{
+    console.log("you should have some ".concat(snack));
+}
+///home work
+//  switch statements//
+//  quiz app //
+// basic funct
---
+++ main.ts
@@ -0,0 +1,50 @@
+
+let isCompleted = true;
+if(isCompleted){
+    console.log("welcome");
+}
+else{console.log("bye")};
+
+
+let age = 18;
+
+if (age >18){
+    console.log("you are allowed");
+
+}
+else{console.log("you are not allowed")}
+
+
+let haveNiceJob = true;
+let canCook = false ;
+if(haveNiceJob && canCook){
+    console.log("rishta pakkA")}
+else{console.log("rejected")};
+
+
+let isHandsome = true;
+let isRich = false;
+
+if(isHandsome || isRich  )
+{
+    console.log("rishta pakka!")
+}
+else{console.log("rejected")}
+
+
+
+let percentage = 44;
+if (percentage >=80 && percentage <=100 ) {
+    console.log("Your grade is A+")}
+else if (percentage >=70 && percentage <80 ){console.log("your grade is A")}
+else if (percentage >=60 && percentage<70 ){console.log("your grade is B")}
+else if (percentage <=50 && percentage <60 ){console.log("your grade is C")}
+else{console.log("your are fail")}
+
+
+////TERNARY OPERATOR 
+
+
+let isHungry = true ;
+ let snack = isHungry? "apple" : "water";{console.log(`you should have some ${snack}`)}
+
